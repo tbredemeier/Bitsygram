836c08af1362671de7a018cb045da79498d50bf1
Tom Bredemeier <tbredemeier@barrington220.org>
2018-04-06 16:25:41 -0500

    added like button functionality

--- Bitsygram/CollectionViewCell.swift
+++ Bitsygram/CollectionViewCell.swift
@@ -12,4 +12,14 @@ class CollectionViewCell: UICollectionViewCell {
 
     @IBOutlet var imageView: UIImageView!
 
+    @IBAction func onLikeButtonTapped(_ sender: UIButton) {
+        if sender.tag == 0 {
+            sender.setImage(UIImage(named: "Like"), for: .normal)
+            sender.tag = 1
+        }
+        else {
+            sender.setImage(UIImage(named: "UnLike"), for: .normal)
+            sender.tag = 0
+        }
+    }
 }
